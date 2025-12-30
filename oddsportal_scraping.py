7c4579d5a25d8061f8a873f010ca2fd6d0fa0143
Karol Wilk-Juraszek <wilkjuraszekkarol@gmail.com>
2025-12-30 23:16:43 +0100

    Add files via upload

--- oddsportal_scraping.py
+++ oddsportal_scraping.py
@@ -6,8 +6,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 
-def scraping(url, x, megalista):
-    service = Service(executable_path="PATH\\chromedriver.exe")
+def scraping(url, x, megalist):
+    service = Service(executable_path="C:\\Users\\Karol\\Desktop\\Algobetting\\projekty-i-w-ogóle\\Scraping i nie tylko\\chromedriver.exe")
     driver = webdriver.Chrome(service=service)
     driver.maximize_window()
     driver.delete_all_cookies()
@@ -19,78 +19,78 @@ def scraping(url, x, megalista):
     finally:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") #Ensures that we actually load the entire site
     time.sleep(2)
-    tabela = driver.find_elements(By.CSS_SELECTOR, css_selector)
-
-    stringi = []
+    table = driver.find_elements(By.CSS_SELECTOR, css_selector)
+    #-------------
+    strings = []
     list = []
-    if x[0] in (13, 14, 15, 16, 17, 18, 19): #Cleaning
-        for mecz in tabela:
-            stringi.append(mecz.text)
-            stringi[-1] = stringi[-1].replace("1\nX\n2\nB","")
-            if stringi[-1].count(f"20{x[0]}") == 1 or stringi[-1].count(f"20{x[0]+1}") == 1:
-                lista = stringi[-1].splitlines(keepends=False)
-                if lista[0].count(" - Grupa mistrzowska") == 1:
-                    lista[0] = lista[0].replace(" - Grupa mistrzowska", "")
-                elif lista[0].count(" - Grupa spadkowa") == 1:
-                    lista[0] = lista[0].replace(" - Grupa spadkowa", "")
+    if x[0] in (13, 14, 15, 16, 17, 18, 19): #Parsing
+        for game in table:
+            strings.append(game.text)
+            strings[-1] = strings[-1].replace("1\nX\n2\nB","")
+            if strings[-1].count(f"20{x[0]}") == 1 or strings[-1].count(f"20{x[0]+1}") == 1:
+                small_list = strings[-1].splitlines(keepends=False)
+                if small_list[0].count(" - Grupa mistrzowska") == 1:
+                    small_list[0] = small_list[0].replace(" - Grupa mistrzowska", "")
+                elif small_list[0].count(" - Grupa spadkowa") == 1:
+                    small_list[0] = small_list[0].replace(" - Grupa spadkowa", "")
                 else:
                     pass
-                lista.remove("")
-                lista.pop(-1)
-                list.append(lista)
+                small_list.remove("")
+                small_list.pop(-1)
+                list.append(small_list)
             else:
-                lista = ["Data jak powyżej"]
-                lista2 = stringi[-1].splitlines(keepends=False)
-                lista = lista + lista2
-                lista.pop(-1)
-                list.append(lista)
-            if lista.count('przyz.') == 1:
-                lista.remove('przyz.')
-            elif lista.count("Dodaj do mojego kuponu") == 1:
-                lista.remove("Dodaj do mojego kuponu")
+                small_list = ["Data jak powyżej"]
+                small_list2 = strings[-1].splitlines(keepends=False)
+                small_list = small_list + small_list2
+                small_list.pop(-1)
+                list.append(small_list)
+            if small_list.count('przyz.') == 1:
+                small_list.remove('przyz.')
+            elif small_list.count("Dodaj do mojego kuponu") == 1:
+                small_list.remove("Dodaj do mojego kuponu")
             else:
                 pass
     else:
-        for mecz in tabela:
-            stringi.append(mecz.text)
-            stringi[-1] = stringi[-1].replace("1\nX\n2\nB","")
-            if stringi[-1].count(f"20{x[0]}") == 1 or stringi[-1].count(f"20{x[0]+1}") == 1:
-                lista = stringi[-1].splitlines(keepends=False)
-                lista.remove("")
-                lista.pop(-1)
-                list.append(lista)
+        for game in table:
+            strings.append(game.text)
+            strings[-1] = strings[-1].replace("1\nX\n2\nB","")
+            if strings[-1].count(f"20{x[0]}") == 1 or strings[-1].count(f"20{x[0]+1}") == 1:
+                small_list = strings[-1].splitlines(keepends=False)
+                small_list.remove("")
+                small_list.pop(-1)
+                list.append(small_list)
             else:
-                lista = ["Data jak powyżej"]
-                lista2 = stringi[-1].splitlines(keepends=False)
-                lista = lista + lista2
-                lista.pop(-1)
-                list.append(lista)
-            if lista.count('przyz.') == 1:
-                lista.remove('przyz.')
-            elif lista.count("Dodaj do mojego kuponu") == 1:
-                lista.remove("Dodaj do mojego kuponu")
+                small_list = ["Data jak powyżej"]
+                small_list2 = strings[-1].splitlines(keepends=False)
+                small_list = small_list + small_list2
+                small_list.pop(-1)
+                list.append(small_list)
+            if small_list.count('przyz.') == 1:
+                small_list.remove('przyz.')
+            elif small_list.count("Dodaj do mojego kuponu") == 1:
+                small_list.remove("Dodaj do mojego kuponu")
             else:
                 pass
 
     list[0] = list[0][5:] #Cleans the first row from unnecessary scraps
 
-    for mecz in list:
+    for game in list:
         try:
-            if int(mecz[3]) > int(mecz[5]):
-                mecz.append("Wygrali gospodarze")
-            elif int(mecz[3]) < int(mecz[5]):
-                mecz.append("Wygrali goście")
+            if int(game[3]) > int(game[5]):
+                game.append("Wygrali gospodarze")
+            elif int(game[3]) < int(game[5]):
+                game.append("Wygrali goście")
             else:
-                mecz.append("Remis")
+                game.append("Remis")
         except ValueError:
-            list.remove(mecz)
+            list.remove(game)
 
     print(len(list)) #Checks if everything actually loaded. Value should be exactly 50 for every page except the last one (for each season)
-    megalista.append(list)
+    megalist.append(list)
     driver.close()
-    return tabela
+    return table
 
-def tworzenie_tabeli(x, megalista):
+def creating_df(x, megalist):
     j = 1
     while j <= x[1]:
         if x[0] == 16:
@@ -99,37 +99,39 @@ def tworzenie_tabeli(x, megalista):
             url = f"https://www.oddsportal.com/pl/football/poland/ekstraklasa-20{x[0]}-20{x[0]+1}/results/#/page/{j}"
         elif x[0] in (19,20,21,22,23,24):
             url = f"https://www.oddsportal.com/pl/football/poland/pko-bp-ekstraklasa-20{x[0]}-20{x[0]+1}/results/#/page/{j}"
-        scraping(url, x, megalista)
+        scraping(url, x, megalist)
         j = j+1
-    megalista = [x for xs in megalista for x in xs]
-    print(megalista)
+    megalist = [x for xs in megalist for x in xs]
+    print(megalist)
 
-    for i in range(len(megalista)):
-        megalista[i].pop(4)
-        if megalista[i][0] == "Data jak powyżej":
-            megalista[i][0] = megalista[i-1][0]
+    for i in range(len(megalist)):
+        megalist[i].pop(4)
+        if megalist[i][0] == "Data jak powyżej":
+            megalist[i][0] = megalist[i-1][0]
         else:
             pass
 
-    df = pd.DataFrame(megalista, columns = ['Data', 'Godzina', 'Gospodarze', 'Gole_gospodarze', 'Gole_goście', 'Goście', 'Oddsy_gospodarze', 'Oddsy_remis', 'Oddsy_goście', 'Kto_wygrał'])
+    df = pd.DataFrame(megalist, columns = ['Data', 'Godzina', 'Gospodarze', 'Gole_gospodarze', 'Gole_goście', 'Goście', 'Oddsy_gospodarze', 'Oddsy_remis', 'Oddsy_goście', 'Kto_wygrał'])
     df.insert(2, 'Sezon', f'{x[0]}/{x[0]+1}')
     print(df.to_string())
     return df
 
+def creating_full_df(list):
+    dfs = []
+    for i in list:
+        megalist = []
+        df = creating_df(i, megalist)
+        dfs.append(df)
+    full_data = dfs[0]
+    for j in range(1, len(dfs)):
+        full_data = pd.concat([full_data, dfs[j]])
+    full_data = full_data.reset_index(drop=True)
+    return full_data
 
 if __name__ == '__main__':
-    lista = [[12, 5], [13,6], [14,6], [15,6], [16,6], [17,6], [18,6], [19,6], [20,5], [21,7], [22,7], [23,7], [24,7]]
-    lista_dfow = []
-    for i in lista:
-        megalista = []
-        df = tworzenie_tabeli(i, megalista)
-        lista_dfow.append(df)
-    lista_dfow.reverse()
-    pelne_dane = lista_dfow[0]
-    for j in range(1, len(lista_dfow)):
-        pelne_dane = pd.concat([pelne_dane, lista_dfow[j]])
-    pelne_dane = pelne_dane.reset_index(drop=True)
-    pelne_dane.to_csv(f"PATH\\pelne_dane_oddsportal.csv", sep=',', encoding='utf-8-sig')
+    values = [[24,7], [23,7], [22,7], [21,7], [20,5], [19,6], [18,6], [17,6], [16,6], [15,6], [14,6], [13,6], [12,5]]
+    full_data = creating_full_df(values)
+    full_data.to_csv(f"C:\\Users\\Karol\\Desktop\\Algobetting-Ekstraklasa_OFFICIAL\\pelne_dane_oddsportal2.csv", sep=',', encoding='utf-8-sig')
 
 
 
